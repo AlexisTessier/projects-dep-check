f6cbf2bbf7ff2e70f7bb4a6cbf09f628113ef207
Alexis Tessier <alexistessierdev@gmail.com>
2018-02-22 18:08:33 +0100

    update dependencies

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 module.exports = [
 	__dirname,
-	path.join(__dirname, '../@alexistessier/msg'),
 	path.join(__dirname, '../@alexistessier/es-lint-config'),
-	path.join(__dirname, '../cleanquirer'),
-	path.join(__dirname, '../files-generator'),
-	path.join(__dirname, '../javascript-value-locator'),
 	path.join(__dirname, '../node-module-starterkit-release-dep'),
-	path.join(__dirname, '../nosg'),
+	path.join(__dirname, '../@alexistessier/msg'),
 	path.join(__dirname, '../stringable'),
-	path.join(__dirname, '../vanille-type')
+	path.join(__dirname, '../vanille-type'),
+	path.join(__dirname, '../javascript-value-locator'),
+	path.join(__dirname, '../files-generator'),
+	path.join(__dirname, '../cleanquirer'),
+	path.join(__dirname, '../nosg')
 ]
